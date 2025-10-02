866932937ab5a15c7cd9f9110a178c08fde52ef9
GabrielRossi01 <rossi17006@gmail.com>
2025-10-02 19:28:40 -0300

    Inicializa projeto Java com Spring Boot e adiciona dependências

---
+++ src/main/java/br/com/fiap/knowball/KnowballApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.knowball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/knowball/KnowballApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.knowball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
