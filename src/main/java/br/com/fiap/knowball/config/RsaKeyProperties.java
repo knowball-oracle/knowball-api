22aa580b5d40457a6f513bc8822cee1aee277ca3
GabrielRossi01 <rossi17006@gmail.com>
2026-03-20 22:53:19 -0300

    renomeando arquivo com nomenclatura correta dentro da aplicação

--- src/main/java/br/com/fiap/knowball/config/RsaKeyProperties.java
+++ src/main/java/br/com/fiap/knowball/config/RsaKeyProperties.java
@@ -6,6 +6,6 @@ import java.security.interfaces.RSAPublicKey;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "rsa")
-public record RsaKeysProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
     
 }
