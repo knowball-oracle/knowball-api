e59007436d23f9c675c4031a53a7b31b01ab5fb9
GabrielRossi01 <rossi17006@gmail.com>
2026-04-07 21:52:17 -0300

    implementa migration de inserir um usuário com role de administrador na tabela users

---
+++ src/main/resources/db/migration/V1__insert_admin.sql
@@ -0,0 +1,7 @@
+INSERT INTO knowball_users (name, email, password, role)
+VALUES (
+    'Admin',
+    'admin@knowball.com',
+    '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2uheWG/igi.',
+    'ROLE_ADMIN');
+COMMIT;
