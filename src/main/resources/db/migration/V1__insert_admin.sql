432d77e986e476d8852468748edf556db71e857e
GabrielRossi01 <rossi17006@gmail.com>
2026-05-21 15:43:56 -0300

    ajustando V1 do flyway para ficar idempotente

--- src/main/resources/db/migration/V1__insert_admin.sql
+++ src/main/resources/db/migration/V1__insert_admin.sql
@@ -1,7 +1,14 @@
 INSERT INTO knowball_users (name, email, password, role)
-VALUES (
+SELECT
     'Admin',
     'admin@knowball.com',
     '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2uheWG/igi.',
-    'ROLE_ADMIN');
+    'ROLE_ADMIN'
+FROM dual
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM knowball_users
+    WHERE email = 'admin@knowball.com'
+);
+
 COMMIT;
